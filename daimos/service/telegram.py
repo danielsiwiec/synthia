3324987ea7f4b3f2833fb54906361c048419c106
Dan Siwiec <daniel.siwiec@gmail.com>
2025-10-10 17:13:29 -0700

    add telegram message handler

--- daimos/service/telegram.py
+++ daimos/service/telegram.py
@@ -1,6 +1,6 @@
 from loguru import logger
 from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
 from daimos.agents.subagents import TaskAgentException
 from daimos.service.task import TaskRequest, TaskService
@@ -12,6 +12,7 @@ class Telegram:
         self.task_service = task_service
         self.application = Application.builder().token(token).build()
         self.application.add_handler(CommandHandler("task", self._task_handler, has_args=True))
+        self.application.add_handler(MessageHandler(callback=self._message_handler, filters=filters.TEXT))
 
     async def send_message(self, message: str):
         await self.application.bot.send_message(text=message, chat_id=self.chat_id, parse_mode="Markdown")
@@ -29,6 +30,7 @@ class Telegram:
         await self.application.shutdown()
 
     async def _task_handler(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        logger.debug(f"Task handler called with context.args: {context.args}")
         if update.message.from_user.id != int(self.chat_id):
             logger.debug(f"Unauthorized user {update.message.from_user.id}")
             return
@@ -38,7 +40,21 @@ class Telegram:
             return
 
         try:
-            result = await self.task_service.process_task(TaskRequest(task=" ".join(context.args)))
+            result = await self.task_service.process_task(TaskRequest(task=" ".join(context.args)), resume=False)
+            await update.message.reply_text(result.result)
+        except TaskAgentException as e:
+            await update.message.reply_text(str(e))
+
+    async def _message_handler(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        if update.message.from_user.id != int(self.chat_id):
+            logger.debug(f"Unauthorized user {update.message.from_user.id}")
+            return
+
+        if not update.message.text:
+            return
+
+        try:
+            result = await self.task_service.process_task(TaskRequest(task=update.message.text), resume=True)
             await update.message.reply_text(result.result)
         except TaskAgentException as e:
             await update.message.reply_text(str(e))
